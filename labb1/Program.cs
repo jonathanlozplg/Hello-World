2864daac38f6c5ab3fb13d8ea820dcd010407ca4
jonathanlozplg <115471019+jonathanlozplg@users.noreply.github.com>
2022-10-12 00:23:37 +0200

    Update Program.cs

--- labb1/Program.cs
+++ labb1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+ string myName = Console.readline();
+
+Console.Wtriteline("Welcome to the world " + myName);
